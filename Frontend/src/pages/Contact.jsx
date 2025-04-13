7fedef299bd0c5cea3ff2336b21f70c2c862e8ae
ShakyaSmriti <119920095+ShakyaSmriti@users.noreply.github.com>
2025-04-13 20:17:29 +0545

    newsletter comp removed from home

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Title from "./../components/Title";
 import { assets } from "../assets/assets";
 
-
 const Contact = () => {
   return (
     <div>
@@ -24,21 +23,15 @@ const Contact = () => {
         <div className="flex flex-col justify-center items-start gap-4 max-w-[350px] self-center">
           <p className="font-semibold text-xl text-gray-600">Our Store</p>
           <p className="text-gray-500">
-             <br /> kuleshwor
+            <br /> kuleshwor
           </p>
           <p>
             Tel: 9865439201 <br /> Email: shakyasmriti368@gmail.com
           </p>
 
-          
-          <p>
-         
-            Kathmandu
-          </p>
-        
+          <p>Kathmandu</p>
         </div>
       </div>
-      
     </div>
   );
 };
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import OurPolicy from "../components/OurPolicy";
-import NewsletterBox from "../components/NewsletterBox";
 
 const Home = () => {
   return (
@@ -10,7 +9,6 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <OurPolicy />
-      <NewsletterBox />
     </div>
   );
 };
