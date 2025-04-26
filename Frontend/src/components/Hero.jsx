11c69bee482acf470567702de533452691efcfb9
ShakyaSmriti <119920095+ShakyaSmriti@users.noreply.github.com>
2025-04-26 13:39:54 +0545

    Style 'SHOP NOW' as a button

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -12,19 +12,24 @@ export default function Hero() {
               STEP INTO FASHION
             </p>
           </div>
-          <h1 className=" prata-regular text-2xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="prata-regular text-2xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
           </h1>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 ">
             <p className="font-semibold text-sm md:text-base">
-              <a href="/collection">SHOP NOW</a>
+              <a
+                href="/collection"
+                className="inline-block px-6 py-3 mt-4 text-white bg-[#414141] hover:bg-[#333333] rounded-full transition duration-300"
+              >
+                SHOP NOW
+              </a>
             </p>
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
           </div>
         </div>
       </div>
-      {/*hero right side */}
-      <img className="w-full sm:w-1/2 h-85 " src={assets.hero_img} alt=""></img>
+      {/* hero right side */}
+      <img className="w-full sm:w-1/2 h-85" src={assets.hero_img} alt="" />
     </div>
   );
 }
